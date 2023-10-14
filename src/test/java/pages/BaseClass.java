0e91e17d9800a93172228b1cca7ae293b6cd7258
Nour Alaa <nour.alaa@vodafone.com>
2023-10-15 01:22:40 +0300

    readme file

--- src/test/java/pages/BaseClass.java
+++ src/test/java/pages/BaseClass.java
@@ -2,21 +2,11 @@ package pages;
 
 import driver.WebDriverSingleton;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-import utilities.CountryDataUtility;
-import utilities.JsonReader;
 import utilities.WaitUtils;
-
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 public class BasePage extends AbstractTestNGCucumberTests {
 
@@ -31,7 +21,8 @@ public class BasePage extends AbstractTestNGCucumberTests {
     }
 
     WebDriver driver = driverSingleton.getWebDriver();
-    WaitUtils wait  = new WaitUtils(driver);;
+    WaitUtils wait = new WaitUtils(driver);
+    ;
 
 
     @BeforeSuite
